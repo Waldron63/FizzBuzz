effda595fa9bce7d1f2b4be2142e764c980e7432
Waldron63 <ingsis.gualdron.santiago@gmail.com>
2026-02-21 12:38:33 -0500

    mayor cobertura de casos bordes

--- src/test/java/com/example/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/com/example/fizzBuzz/FizzBuzzTest.java
@@ -23,4 +23,29 @@ public class FizzBuzzTest {
     void testFizzBuzzMultiplo3y5() {
         assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
     }
+
+    @Test
+    void testFizzBuzzMultiplo0() {
+        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(0));
+    }
+
+    @Test
+    void testFizzBuzzMultiplo3Negativo() {
+        assertEquals("Fizz", FizzBuzz.fizzbuzz(-3));
+    }
+
+    @Test
+    void testFizzBuzzMultiplo5Negativo() {
+        assertEquals("Buzz", FizzBuzz.fizzbuzz(-5));
+    }
+
+    @Test
+    void testFizzBuzzMultiplo3y5Negativo() {
+        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
+    }
+
+    @Test
+    void testFizzBuzzReturnsNegativeNumber() {
+        assertEquals("-4", FizzBuzz.fizzbuzz(-4));
+    }
 }
